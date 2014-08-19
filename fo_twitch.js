26f257fd3e45233d6b9764243c77b8f231565fb5
FO-nTTaX <fo.nttax@yahoo.de>
2014-08-19 23:33:54 +0200

    Create fo_twitch.js

---
+++ fo_twitch.js
@@ -0,0 +1,22 @@
+/*
+ * copyright 2014 - FO-nTTaX
+ * see the license file for licensing information
+ */
+
+function fogetstreams () {
+  $('.fotwitchstreams').children('.fotwitchstream').each(
+    function () {
+      var queryString = "fo_twitch.php?stream="+$(this).children('input.fotwitchstreamvalue').val();
+      var currentElement = this;
+      $.getJSON(queryString, function(data) {
+        if (data.stream != null) {
+          $(currentElement).children('span.fotwitchstreamlive').text("Online");
+        } else {
+          $(currentElement).children('span.fotwitchstreamlive').text("Offline");
+        }
+      });
+    }
+  );
+}
+
+fogetstreams();
