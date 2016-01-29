b2cc8d84fed67cc34b5a48855f39608347f5d393
Alex Winkler <FO-nTTaX@users.noreply.github.com>
2016-01-29 06:34:46 +0100

    Update fo_twitch.js

--- fo_twitch.js
+++ fo_twitch.js
@@ -3,20 +3,20 @@
  * see the license file for licensing information
  */
 
-function fogetstreams () {
-  $('.fotwitchstreams').children('.fotwitchstream').each(
-    function () {
-      var queryString = "fo_twitch.php?stream="+$(this).children('input.fotwitchstreamvalue').val();
-      var currentElement = this;
-      $.getJSON(queryString, function(data) {
-        if (data.stream != null) {
-          $(currentElement).children('span.fotwitchstreamlive').text("Online");
-        } else {
-          $(currentElement).children('span.fotwitchstreamlive').text("Offline");
-        }
-      });
-    }
-  );
-}
-
-fogetstreams();
+$(document).ready(
+  function() {
+    $('.fotwitchstreams').children('.fotwitchstream').each(
+      function() {
+        var queryString = "fo_twitch.php?stream=" + $(this).children('input.fotwitchstreamvalue').val();
+        var currentElement = this;
+        $.getJSON(queryString, function(data) {
+          if(data.stream != null) {
+            $(currentElement).children('span.fotwitchstreamlive').text("Online");
+          } else {
+            $(currentElement).children('span.fotwitchstreamlive').text("Offline");
+          }
+        });
+      }
+    );
+  }
+);
